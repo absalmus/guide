0be75d2779caa5aa75c8623e9cfdd35c30a1c798
absalmus <absalmus99@gmail.com>
2026-06-15 19:12:20 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // Enable core library desugaring required by some plugins (e.g. flutter_local_notifications)
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -43,4 +45,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Desugaring library required for newer Android language features used by some plugins
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
